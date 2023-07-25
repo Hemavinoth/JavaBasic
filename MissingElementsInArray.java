405dcb04b395c6985b8748511b2dde752ec4d54d
hemalatha <avhemavinoth@gmail.com>
2023-07-25 18:31:41 +0530

    Update MissingElementsInArray.java

--- MissingElementsInArray.java
+++ MissingElementsInArray.java
@@ -6,21 +6,27 @@ public class MissingElementsInArray {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 1,3,4,6,7};
+		int[] arr = { 1, 4, 3, 2, 6, 7 };
 		Arrays.sort(arr);
-		
+		// 123467
 		int length = arr.length;
+		// get the size of the array
+		System.out.println(length); // 123467 // count 6
 
-		for (int i = arr[0]; i < length; i++) {
-			
+		for (int i = 0; i < length; i++)// 0<6 
+			{
 
-			if (i != arr[i]) {
+			if (arr[i] != i + 1) {
+              // 1!=1 false
+				
+              //2!=2 false
+				
+				
 
-				System.out.println(arr[i]);
+				System.out.println(i + 1);
 				break;
 
 			}
-			arr[i]++;
 
 		}
 	}
